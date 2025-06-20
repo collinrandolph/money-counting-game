f82e5aa83620a4b3e29cc8379eed032c35fb08f9
collinrandolph <29718220+collinrandolph@users.noreply.github.com>
2025-06-19 21:35:21 -0500

    Add visuals interface, and refactor game logic

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
-let currentSolution;
+window.onload = () => {
+    document.getElementById('answer-submit').onclick = ans;
+    go();
+}
+
+let problemSolution;
 let problemString;
 
 const PROPERTIES = {
@@ -31,7 +36,7 @@ let getProperty = (propList, excludedProp) => {
     let selectedProperty = excludedProp;
 
     while (selectedProperty == excludedProp) {
-        selectedProperty = propertyList[getRandomInteger(0, propertyList.length)];
+        selectedProperty = propertyList[getRandomInteger(0, propertyList.length - 1)];
     }
 
     return selectedProperty;
@@ -51,28 +56,40 @@ let getDollarAmount = (val) => {
 }
 
 // Return a randomized assortment of coins based on a passed value.
+// let makeChange = (val) => {
+
+//     let coinObject = {};
+
+//     coinObject.dollars = getRandomInteger(0, Math.floor(val / 100) - 1);
+//     coinObject.quarters = getRandomInteger(0, Math.floor((val - 100 * coinObject.dollars) / 25));
+//     coinObject.dimes = getRandomInteger(0, Math.floor((val - 100 * coinObject.dollars - 25 * coinObject.quarters) / 10));
+//     coinObject.nickels = (val - 100 * coinObject.dollars - 25 * coinObject.quarters - 10 * coinObject.dimes) / 5;
+
+//     return (coinObject);
+
+// }
+
 let makeChange = (val) => {
+    let coinObject = {};
 
-    let changeObject = {};
+    coinObject.dollars =  getRandomInteger(Math.floor(val / 300), Math.floor(val / 100));
+    coinObject.quarters = getRandomInteger(Math.floor((val - 100 * coinObject.dollars) / 50), Math.floor((val - 100 * coinObject.dollars) / 25));
+    coinObject.dimes = getRandomInteger(Math.floor((val - 100 * coinObject.dollars - 25 * coinObject.quarters) / 30), Math.floor((val - 100 * coinObject.dollars - 25 * coinObject.quarters) / 10));
+    coinObject.nickels = (val - 100 * coinObject.dollars - 25 * coinObject.quarters - 10 * coinObject.dimes) / 5;
 
-    changeObject.dollars = getRandomInteger(0, Math.floor(val / 100) - 1);
-    changeObject.quarters = getRandomInteger(0, Math.floor((val - 100 * changeObject.dollars) / 25));
-    changeObject.dimes = getRandomInteger(0, Math.floor((val - 100 * changeObject.dollars - 25 * changeObject.quarters) / 10));
-    changeObject.nickles = (val - 100 * changeObject.dollars - 25 * changeObject.quarters - 10 * changeObject.dimes) / 5;
-
-    return generateCoinString(changeObject);
+    return (coinObject);
 
 }
 
 // Convert object of coin quantities into a descriptive string using plain english.
-let generateCoinString = (changeObject) => {
+let generateCoinString = (coinObject) => {
 
     let coinString = "";
 
     //Determine the appropriate separator string by evaluating if coins denominations before and after are at zero quantity
     let makeSeparator = (denomination) => {
 
-        if ((denomination == "quarters" && changeObject.dimes == 0 && changeObject.nickles == 0 && coinString.length > 0) || (denomination == "dimes" && changeObject.nickles == 0 && coinString.length > 0) || (denomination == "nickles" && coinString.length > 0)) {
+        if ((denomination == "quarters" && coinObject.dimes == 0 && coinObject.nickels == 0 && coinString.length > 0) || (denomination == "dimes" && coinObject.nickels == 0 && coinString.length > 0) || (denomination == "nickels" && coinString.length > 0)) {
             return ", and "
         } else if (coinString.length > 0) {
             return ", "
@@ -82,71 +99,121 @@ let generateCoinString = (changeObject) => {
     }
 
     //Assemble final coin string
-    if (changeObject.dollars > 0) {
-        coinString += changeObject.dollars + " dollars";
+    if (coinObject.dollars > 0) {
+        coinString += coinObject.dollars + " dollars";
     }
-    if (changeObject.quarters > 0) {
-        coinString += makeSeparator("quarters") + changeObject.quarters + " quarters";
+    if (coinObject.quarters > 0) {
+        coinString += makeSeparator("quarters") + coinObject.quarters + " quarters";
     }
-    if (changeObject.dimes > 0) {
-        coinString += makeSeparator("dimes") + changeObject.dimes + " dimes";
+    if (coinObject.dimes > 0) {
+        coinString += makeSeparator("dimes") + coinObject.dimes + " dimes";
     }
-    if (changeObject.nickles > 0) {
-        coinString += makeSeparator("nickles") + changeObject.nickles + " nickles"
+    if (coinObject.nickels > 0) {
+        coinString += makeSeparator("nickels") + coinObject.nickels + " nickels"
     }
 
     return coinString;
 
 }
 
+let renderCoins = (coinObject, name) => {
+    let walletGroupString;
+    console.log(coinObject);
+    let renderCoinString = "";
+
+    for (let i = 0; i < coinObject.dollars; i++) {
+        renderCoinString += `<div id="${name}-dollar-${i + 1}" data-value="100" class="money bill dollar">$1</div>`
+    }
+    for (let i = 0; i < coinObject.quarters; i++) {
+        renderCoinString += `<div id="${name}-quarter-${i + 1}" data-value="25" class="money coin quarter">25</div>`
+    }
+    for (let i = 0; i < coinObject.dimes; i++) {
+        renderCoinString += `<div id="${name}-dime-${i + 1}" data-value="10" class="money coin dime">10</div>`
+    }
+    for (let i = 0; i < coinObject.nickels; i++) {
+        renderCoinString += `<div id="${name}-nickel-${i + 1}" data-value="10" class="money coin nickel">5</div>`
+    }
+
+    walletGroupString = `<div id="${name}-wallet" class="wallet">${name}${renderCoinString}</div>`
+
+    return (walletGroupString);
+}
+
+let renderWallets = (string) => {
+    document.getElementById("wallet-container").innerHTML = string;
+}
+
+let updateProblem = (string) => {
+    document.getElementById("problem-container").innerHTML = string;
+}
+
 let addition_generateProblems = () => {
     let problemSelect = getRandomInteger(1, 2);
     if (problemSelect == 1) {
         return addition_buyingFruit();
-    }else if (problemSelect ==2){
+    } else if (problemSelect == 2) {
         return addition_sharingFriends();
     }
 }
 
 let addition_buyingFruit = () => {
 
-    let problemSum = getRandomInteger(1, 200) * 5;
+    let problemSum = getRandomInteger(1, 100) * 5;
     let problemFactorOne = getRandomInteger(1, problemSum / 5) * 5;
     let problemFactorTwo = problemSum - problemFactorOne;
     let nameOne = getProperty(PROPERTIES.name);
     let fruitName = getProperty(PROPERTIES.fruit);
 
+    let coinObjectOne = makeChange(problemFactorOne);
+
     problemSolution = problemFactorTwo;
 
-    problemString = `${nameOne} wants to buy a ${fruitName} that costs ${getDollarAmount(problemSum)}. ${nameOne} has ${makeChange(problemFactorOne)}. How much more money does ${nameOne} need to buy ${fruitName}?`
+    problemString = `${nameOne} wants to buy ${fruitName} that costs ${getDollarAmount(problemSum)}. ${nameOne} has ${generateCoinString(coinObjectOne)}. How much more money does ${nameOne} need to buy ${fruitName}?`;
 
-    return problemString;
+    const walletString = renderCoins(coinObjectOne, nameOne);
+    renderWallets(walletString);
+
+
+    updateProblem(problemString);
 
 }
 
 let addition_sharingFriends = () => {
 
-    let problemFactorOne = getRandomInteger(1, 60) * 5;
-    let problemFactorTwo = getRandomInteger(1, 60) * 5;
+    let problemFactorOne = getRandomInteger(1, 40) * 5;
+    let problemFactorTwo = getRandomInteger(1, 40) * 5;
     let problemFactorSum = problemFactorOne + problemFactorTwo;
     let nameOne = getProperty(PROPERTIES.name);
     let nameTwo = getProperty(PROPERTIES.name, nameOne);
-    
-    problemSolution = problemFactorSum;    
 
-    problemString = `${nameOne} has ${makeChange(problemFactorOne)}, and ${nameTwo} has ${makeChange(problemFactorTwo)}. How much money do they have together?`
+    let coinObjectOne = makeChange(problemFactorOne);
+    let coinObjectTwo = makeChange(problemFactorTwo);
 
-    return problemString;
+    problemSolution = problemFactorSum;
+
+    problemString = `${nameOne} has ${generateCoinString(coinObjectOne)}, and ${nameTwo} has ${generateCoinString(coinObjectTwo)}. How much money do they have together?`;
+
+    let walletString = renderCoins(coinObjectOne, nameOne);
+    walletString += renderCoins(coinObjectTwo, nameTwo);
+    renderWallets(walletString);
+
+
+    updateProblem(problemString);
 
 }
 
 //Return user answer and compare it to the solution of the current problem.
-let ans = (val) => {
+
+
+
+let ans = () => {
+    let val = document.getElementById('answer-field').value;
     if (val == problemSolution) {
         console.log(getProperty(PROPERTIES.affirmation) + " That's correct! The answer is " + getDollarAmount(problemSolution));
-        return go();
+        document.getElementById('answer-field').value = null;
     } else {
         console.log("Try Again!");
         return problemString;
     }
+    return go();
 }
